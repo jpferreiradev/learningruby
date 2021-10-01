2c0140f473d15df704c6510f8cd194852137025a
JP Ferreira <jotepauder01@gmail.com>
2021-10-01 15:17:55 -0300

    Update curso de Ruby

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,30 +1,67 @@
 puts "Bem vindo ao jogo da adivinhação"
 puts "Qual é o seu nome?"
 nome = gets 
-
-puts
-puts
-puts
-puts
-puts
+a
+puts "\n\n\n\n"
 puts "Começaremos o jogo pra você, " + nome 
 
 puts "Escolhendo um número secreto entre 0 e 200..." 
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 
-puts
-puts
-puts
-puts
+puts "\n\n\n"
 puts "Tentativa 1"
 puts "Entre com o número"
 chute = gets
 puts " Será que acertou? Você chutou " + chute 
-puts chute.to_i == numero_secreto
 
+acertou = numero_secreto == chute.to_i
 
+if acertou 
+    puts "Acertou!"
+else 
+    maior = numero_secreto > chute.to_i
+    if maior 
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end 
+end 
 
+puts "\n\n\n"
+puts "Tentativa 2"
+puts "Entre com o número"
+chute = gets
+puts " Será que acertou? Você chutou " + chute 
 
+acertou = numero_secreto == chute.to_i
 
+if acertou 
+    puts "Acertou!"
+else 
+    maior = numero_secreto > chute.to_i
+    if maior 
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end 
+end 
 
+puts "\n\n\n"
+puts "Tentativa 3"
+puts "Entre com o número"
+chute = gets
+puts " Será que acertou? Você chutou " + chute 
+
+acertou = numero_secreto == chute.to_i
+
+if acertou 
+    puts "Acertou!"
+else 
+    maior = numero_secreto > chute.to_i
+    if maior 
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end 
+end 
