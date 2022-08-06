d6f19ad386074cbd9249c4ebc22f0ac92e9aee6a
JP Ferreira <jotepauder01@gmail.com>
2022-08-06 14:30:33 -0300

    Update curso Ruby

---
+++ ruby-poo/entrada_e_saida.rb
@@ -0,0 +1,12 @@
+puts "Olá"
+puts "Digite o seu nome: "
+v1 = gets.chomp 
+puts "O seu nome é " + v1
+puts "Digite o seu sobrenome: "
+v2 = gets.chomp
+puts "O seu sobronme é" + v2 
+puts "O seu nome completo é: " + v1 + v2
+puts "Digite a sua idade:"
+v3 = gets.chomp
+puts "A sua idade é " + v3
+puts "O seu nome é completo é " + v1 + v2 + " e a idade é " + v3 + " anos!" 
--- ruby-poo/ola_mundo.rb
+++ ruby-poo/ola_mundo.rb
@@ -1,2 +1,8 @@
 puts "Olá mundo!"
-puts "#{1+2}"
+puts "#{1+2}" 
+
+v1 = 4
+puts v1
+v2 = 5.6
+puts v2
+
