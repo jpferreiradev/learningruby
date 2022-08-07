d31fbbb657d4fe965ccd7abc85525616f17dccf2
JP Ferreira <jotepauder01@gmail.com>
2022-08-07 16:14:17 -0300

    Update curso Ruby

--- ruby-poo/entrada_e_saida.rb
+++ ruby-poo/entrada_e_saida.rb
@@ -1,12 +1,10 @@
-puts "Olá"
-puts "Digite o seu nome: "
-v1 = gets.chomp 
+=begin
+puts "Olá!"
+puts "Digite o seu nome:"
+v1 = gets.chomp # Essa limja serve para ler a entrada do teclado
 puts "O seu nome é " + v1
-puts "Digite o seu sobrenome: "
-v2 = gets.chomp
-puts "O seu sobronme é" + v2 
-puts "O seu nome completo é: " + v1 + v2
+=end
+
 puts "Digite a sua idade:"
-v3 = gets.chomp
-puts "A sua idade é " + v3
-puts "O seu nome é completo é " + v1 + v2 + " e a idade é " + v3 + " anos!" 
+v2 = gets.chomp
+puts "Sua idade é:" + v2
