be0c50d411843e3409d06747c02c1258bf99460c
JP Ferreira <jotepauder01@gmail.com>
2022-08-08 17:18:42 -0300

    Update curso Ruby

---
+++ ruby-poo/coercao.rb
@@ -0,0 +1,4 @@
+puts "Digite a sua idade:"
+v1 = gets.chomp.to_i #Encadear atráves do ponto, varios métodos
+v2 = v1 + 1
+puts "Sua idade ano que vem será: #{v2}"
---
+++ ruby-poo/strings.rb
@@ -0,0 +1,7 @@
+v1 = 'jp'
+puts "Hello!" + v1
+
+v2 = "jp ferreira" # Interpolação de variaveis
+puts "Beleza? #{v2}"
+
+puts "A soma de 1 + 2 é: #{1 + 2}"
